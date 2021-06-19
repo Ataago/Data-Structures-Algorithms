82f295a969d46b6112130be7041ba59ddbcecd71
ataago <ataago7@gmail.com>
2021-06-19 21:24:28 +0530

    Go board game

---
+++ src/recursions/013_recursion_black_white_board.py
@@ -0,0 +1,57 @@
+# !/usr/bin/env python3
+# encoding: utf-8
+
+"""
+    Find if a given black is conquered in the board game Go.
+        black and white play alternately.
+        black is conquered if it is (and all its neighboring blacks, if any, are also) surrounded by whites or borders of the board.
+        board size is 19x19
+
+    @author: Mohammed Ataaur Rahaman
+"""
+
+BLACK = 'Black'
+WHITE = 'White'
+EMPTY = None
+
+BOARD = [
+    [EMPTY, WHITE, BLACK],
+    [EMPTY, WHITE, BLACK],
+    [EMPTY, EMPTY, EMPTY],
+]
+ROW = len(BOARD)
+COL = len(BOARD[0])
+
+
+def is_captured(i, j, visited):
+    if visited[i][j]:
+        return True
+
+    if BOARD[i][j] == EMPTY:
+        return False
+
+    if BOARD[i][j] == WHITE:
+        return True
+
+    visited[i][j] = True
+    # Move left
+    if j > 0 and not is_captured(i, j - 1, visited):
+        return False
+
+    # Move Right
+    if j < COL - 1 and not is_captured(i, j + 1, visited):
+        return False
+
+    # Move Up
+    if i > 0 and not is_captured(i - 1, j, visited):
+        return False
+
+    # Move Down
+    if i < ROW - 1 and not is_captured(i + 1, j, visited):
+        return False
+
+    return True
+
+if __name__ == '__main__':
+    visited = [[False]*COL for i in range(ROW)]
+    print(is_captured(0, 2, visited=visited))
